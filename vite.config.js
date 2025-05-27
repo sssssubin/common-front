9d20c7a7084edb28c18d0fa2ce938589604858dc
sssssubin <bstnqls@naver.com>
2025-05-27 15:11:36 +0900

    vite.config.js 수정

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,23 @@
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [vue()],
   build: {
     target: "node18",
     lib: {
-      entry: path.resolve(__dirname, "src/index.js"),
+      entry: resolve(__dirname, "src/index.js"),
       name: "CommonFront",
       formats: ["es", "cjs"],
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
+      external: ["vue"], // vue는 외부에서 주입 (중복 번들 방지)
       output: {
         globals: {
           vue: "Vue",
