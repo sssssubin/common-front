8cb4412c1947955edc65f9c96601c8d6febdcbfe
sssssubin <bstnqls@naver.com>
2025-05-27 15:03:24 +0900

    package.json, vite.config.js 수정

--- vite.config.js
+++ vite.config.js
@@ -4,25 +4,14 @@ import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      "@common-front": path.resolve(
-        __dirname,
-        "node_modules/@sssssubin/common-front/dist"
-      ),
-    },
-  },
-  optimizeDeps: {
-    include: ["@sssssubin/common-front"],
-  },
   build: {
+    target: "node18",
     lib: {
       entry: path.resolve(__dirname, "src/index.js"),
       name: "CommonFront",
       formats: ["es", "cjs"],
       fileName: (format) => `index.${format}.js`,
     },
-    cssCodeSplit: false, // 모든 CSS 하나로 묶기
     rollupOptions: {
       output: {
         globals: {
